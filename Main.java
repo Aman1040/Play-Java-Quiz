edbdc04c15d96aaa2097826435109371592cda99
Aman Siriah <132004836+Aman1040@users.noreply.github.com>
2024-03-11 07:12:24 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        QuestionServices qServices = new QuestionServices();
+        qServices.displayQuestions();
+        qServices.playQuiz();
+        qServices.printScore();
+
+    }
+
+}
